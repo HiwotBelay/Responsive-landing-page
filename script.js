423db4945f2be18c8e33f04325cb606097b11a35
Hiwot Belay <104851504+HiwotBelay@users.noreply.github.com>
2024-09-14 15:53:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add event listener to change nav background on scroll
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
